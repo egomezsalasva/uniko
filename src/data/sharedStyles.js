e483e65876112654f39451b26c014185223dbcca
egomezsalasva <47556145+egomezsalasva@users.noreply.github.com>
2019-09-27 15:01:08 +0200

    Updated Styles and DB

--- src/data/sharedStyles.js
+++ src/data/sharedStyles.js
@@ -10,12 +10,14 @@ const unikoGrey = "#E8E6E6"
 
 
 //Widths
-const w40px = "3.125vw"
+//const w40px = "3.125vw"
+const w40px = "2.77777778vw"
 const w50px = "3.90625vw"
 const w100px = "7.8125vw"
 const w120px = "9.375vw"
 const w150px = "11.71875vw"
 const w430px = "33.59375vw"
+const w1360px = "94.444444vw"
 
 
 //Heights
@@ -23,7 +25,9 @@ const h30px = "4.28571429vh"
 const h35px = "5vh"
 const h40px = "5.71428571vh"
 const h70px = "10vh"
+const h80px = "10vh"
 const h100px = "14.2857143vh"
+const h110px = "13.75vh"
 const h120px = "17.1428571vh"
 const h130px = "18.5714285714vh"
 const h160px = "22.8571428571vh"
@@ -33,8 +37,8 @@ const h240px = "34.2857143vh"
 
 export { 
     unikoBlack, unikoWhite, unikoGrey,
-    w40px, w50px, w100px, w120px, w150px, w430px,
-    h30px, h35px, h40px, h70px, h100px, h120px, h130px, h160px, h170px, h190px, h240px,
+    w40px, w50px, w100px, w120px, w150px, w430px, w1360px,
+    h30px, h35px, h40px, h70px, h80px, h100px, h110px, h120px, h130px, h160px, h170px, h190px, h240px,
 }
 
 //Z-INDEX CHART
