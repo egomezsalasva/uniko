9edc932315f402665eb351aad5598282623c092a
egomezsalasva <47556145+egomezsalasva@users.noreply.github.com>
2019-11-26 10:17:09 +0100

    Upload 19/11/26 V1 (About & Contact Pages)

---
+++ AboutUs.js
@@ -0,0 +1,23 @@
+//Library Imports
+import React from 'react'
+//Component Imports
+import LogoBlack from '../components/header/logo/LogoBlack'
+//Import Component
+import About from '../components/aboutUs/About'
+
+
+//Main Component
+function AboutUs() {
+    return (
+        <>
+        <LogoBlack/>
+        
+        <About/>  
+
+        </>
+    )
+}
+
+
+//Export
+export default AboutUs;
---
+++ Contact.js
@@ -0,0 +1,23 @@
+//Library Imports
+import React from 'react'
+//Component Imports
+import LogoBlack from '../components/header/logo/LogoBlack'
+//Import Component
+import ContactComp from '../components/contact/ContactComp'
+
+
+//Main Component
+function Contact() {
+    return (
+        <>
+        <LogoBlack/>
+        
+        <ContactComp/>  
+
+        </>
+    )
+}
+
+
+//Export
+export default Contact;
